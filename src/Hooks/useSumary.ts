a6aab6e43c2a536407e1a758aa5faa41d2875896
paulolima1212 <plima12121984@gmail.com>
2022-12-23 23:27:53 +0000

    finished project

--- src/Hooks/useSumary.ts
+++ src/Hooks/useSumary.ts
@@ -1,29 +1,32 @@
 import { useTransaction } from './useTransactionContext';
+import { useMemo } from 'react';
 
 export function useSumary() {
   const { transactions } = useTransaction();
 
-  const sumary = transactions.reduce(
-    (acc, transaction) => {
-      switch (transaction.type) {
-        case 'income':
-          acc.income += transaction.price;
-          acc.total += transaction.price;
-          break;
-        case 'outcome':
-          acc.outcome += transaction.price;
-          acc.total -= transaction.price;
-          break;
-      }
+  const sumary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+            break;
+          case 'outcome':
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+            break;
+        }
 
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    );
+  }, [transactions]);
 
   return sumary;
 }
